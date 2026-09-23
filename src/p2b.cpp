414036dba9871becf49fa1d54697985e6d2c2636
emery-collab <eptherou@ualberta.ca>
2026-09-22 20:23:25 -0600

    small change in p2b.cpp

--- src/p2b.cpp
+++ src/p2b.cpp
@@ -1,1 +1,22 @@
-int main() { return 0; }
+#include <random> // mt19937_64 mersenne twister initialize with std::mtd19937_64 rng(0)
+#include <cstdint> // uint64_t for rng to return
+
+#include <chrono> // timer
+#include <vector> // arrays
+#include <iostream> // std::cout
+//#include <
+
+
+int main() { 
+    
+    
+    constexpr std::size_t numElements = 16'000'000;
+
+    std::vector<uint64_t> array1(numElements);
+    std::vector<uint64_t> array2(numElements);
+    
+    
+    
+    
+    return 0; 
+}
